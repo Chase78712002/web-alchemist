a823fb272aee1635802c4e5a17aa2a2af2be4827
Alvin Lin <alvinch.lin@gmail.com>
2021-07-26 15:37:57 -0700

    Wrap layout component around all the pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
     return (
-        <div>
-            <h2>About page</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores voluptates tenetur consequatur ut. Fugiat labore autem aspernatur, culpa odit, magni porro neque sed amet eum minus aperiam, esse inventore</p>
-        </div>
+        <Layout>
+            <div>
+                <h2>About page</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores voluptates tenetur consequatur ut. Fugiat labore autem aspernatur, culpa odit, magni porro neque sed amet eum minus aperiam, esse inventore</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores voluptates tenetur consequatur ut. Fugiat labore autem aspernatur, culpa odit, magni porro neque sed amet eum minus aperiam, esse inventore</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores voluptates tenetur consequatur ut. Fugiat labore autem aspernatur, culpa odit, magni porro neque sed amet eum minus aperiam, esse inventore</p>
+            </div>
+        </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <section>
-      <Navbar/>
-      <div>
-        <h2>Design</h2>
-        <h3>Develop & deploy</h3>
-        <p>Web developer based in Vancouver</p>
-      </div>
-    </section>
+    <Layout>
+      <section>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & deploy</h3>
+          <p>Web developer based in Vancouver</p>
+        </div>
+      </section>
+    </Layout>
   
     )
 }
