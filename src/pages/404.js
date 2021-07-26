7e8d80be11fa3d786c8be5aa69a4c72408cfe4f1
Alvin Lin <alvinch.lin@gmail.com>
2021-07-26 15:15:27 -0700

    Create a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PageNotFound() {
+    return (
+        <div>
+            <h2>404</h2>
+            <h3>PAGE NOT FOUND</h3>
+            <p>Sorry, that page doesn't exist!!!</p>
+            <p>Are you sure you entered the address correctly?</p>
+        </div>
+    )
+}
