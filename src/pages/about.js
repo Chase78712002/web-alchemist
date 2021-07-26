de8e996cbe1b32eb79d83c5edce25c59ecc5ebde
Alvin Lin <alvinch.lin@gmail.com>
2021-07-26 14:41:10 -0700

    Set up an about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h2>About page</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores voluptates tenetur consequatur ut. Fugiat labore autem aspernatur, culpa odit, magni porro neque sed amet eum minus aperiam, esse inventore</p>
+        </div>
+    )
+}
