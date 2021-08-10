164ba91131624a1c4f7f13585657f6517c88dc43
Alvin Lin <alvinch.lin@gmail.com>
2021-08-09 17:18:30 -0700

    Wrap layout component to  404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function PageNotFound() {
     return (
-        <div>
-            <h2>404</h2>
-            <h3>PAGE NOT FOUND</h3>
-            <p>Sorry, that page doesn't exist!!!</p>
-            <p>Are you sure you entered the address correctly?</p>
-        </div>
+        <Layout>
+            <div>
+                <h2>404</h2>
+                <h3>PAGE NOT FOUND</h3>
+                <p>Sorry, that page doesn't exist!!!</p>
+                <p>Are you sure you entered the address correctly?</p>
+            </div>
+        </Layout>
     )
 }
