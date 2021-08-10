993c8a5d05d6cc60c9ab9e89ebad1a752e820402
Alvin Lin <alvinch.lin@gmail.com>
2021-08-10 15:40:36 -0700

    Import home module css to home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from '../styles/home.module.css'
+
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & deploy</h3>
           <p>Web developer based in Vancouver</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
